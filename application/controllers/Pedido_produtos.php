5fc4f750beaacaa7c44001bc152b4ac62016be72
Luciano <luciano.vittoretti@gmail.com>
2023-04-10 13:05:49 -0300

    - arrumar decimal para formato brasileiro na validacao e no formulario

--- application/controllers/Pedido_produtos.php
+++ application/controllers/Pedido_produtos.php
@@ -27,18 +27,25 @@ class Pedido_produtos extends CI_Controller
         $id = $this->uri->segment(3);
         $dados["id"] = $id;
 
+        // por padrao campos do formulario nao estao limpos
+        $dados["campos_limpos"] = false;
+
         // recupera o pedido do banco de dados
         $dados["pedido"] = $this->Pedidos_model->getPedido($id);
 
+        // recupera os produtos do pedido
+        $dados["pedidos_produtos"] = $this->Pedidos_produtos_model->getProdutos($id);
+
+        // recupera os produtos para o select
         $dados["produtos"] = produtos_to_select($this->Produtos_model->getProdutosAtivos());
 
-        // verifica o retorno
+        // verifica se encontrou o pedido
         if (!$dados["pedido"]) {
             // erro 404
             show_404();
         }
 
-        // verifica o retorno
+        // se pedido ja foi finalizado mostra erro 404
         if ($dados["pedido"]->finalizado) {
             // erro 404
             show_404();
@@ -48,14 +55,21 @@ class Pedido_produtos extends CI_Controller
         $this->form_validation->set_rules('pedidos_id', 'Pedido', 'required');
         $this->form_validation->set_rules('produtos_id', 'Produto', 'required');
         $this->form_validation->set_rules('quantidade', 'Quantidade', 'required|integer');
-        $this->form_validation->set_rules('preco', 'Valor Unitário', 'required|numeric');
+        $this->form_validation->set_rules('preco', 'Valor Unitário', 'required');
 
         // verifica se o formulario foi submetido
         if (!$this->form_validation->run()) {
             $dados['msg'] = validation_errors();
 
-            // recupera os produtos do pedido
-            $dados["pedidos_produtos"] = $this->Pedidos_produtos_model->getProdutos($id);
+            $this->load->view('Pedido_Produtos', $dados);
+            return;
+        }
+
+        // verifica se é decimal formato brasileiro
+        $preco = $this->input->post('preco');
+        $preco = str_replace(",", ".", $preco);
+        if (!is_numeric($preco)) {
+            $dados['msg'] = "Preço inválido. Utilize o formato 0,00.";
 
             $this->load->view('Pedido_Produtos', $dados);
             return;
@@ -66,14 +80,17 @@ class Pedido_produtos extends CI_Controller
             'pedidos_id' => $this->input->post('pedidos_id'),
             'produtos_id' => $this->input->post('produtos_id'),
             'quantidade' => $this->input->post('quantidade'),
-            'preco' => $this->input->post('preco'),
+            'preco' => $preco,
         ];
 
         if ($dados_pedidos_produtos['produtos_id'] &&
             $dados_pedidos_produtos['quantidade'] &&
-            $dados_pedidos_produtos['preco']) {
+            $preco) {
             // insere no banco de dados
             $this->Pedidos_produtos_model->insert($dados_pedidos_produtos);
+
+            // apaga campos do formulario quando inserir
+            $dados["campos_limpos"] = true;
         }
 
         // recupera os produtos do pedido
