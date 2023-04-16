0e438684bad8fa22ee1bb863ffbca7a664d9ae10
Luciano <luciano.vittoretti@gmail.com>
2023-04-16 13:18:06 -0300

    new way to get token to validate session

--- application/helpers/api_helper.php
+++ application/helpers/api_helper.php
@@ -5,20 +5,32 @@ use Restserver\Libraries\REST_Controller;
 function check_authorization() {
     $ci = &get_instance();
 
+    // verificando se token foi enviado
     $headers = $ci->input->request_headers();
-
-    if (empty($headers['jwt-authorization'])) {
+    if (empty($headers['Jwt-Authorization'])) {
         $ci->response([
             'status' => FALSE,
             'message' => 'Token de autorização não está presente no header da requisição (jwt-authorization).'
         ], REST_Controller::HTTP_UNAUTHORIZED);
     }
 
-    $valid_token = $ci->authorization_token->validateToken();
-    if ($valid_token['status'] !== TRUE) {
+    // verificando se token é válido
+    $token_validation = $ci->authorization_token->validateToken();
+    if ($token_validation['status'] === FALSE) {
         $ci->response([
             'status' => FALSE,
-            'message' => 'Token de autorização inválido.'
+            'message' => 'Token de autorização inválido ou expirado.'
+        ], REST_Controller::HTTP_UNAUTHORIZED);
+    }
+
+    // verificando se usuário está ativo
+    $user_id = $token_validation["data"]->id;
+    $ci->load->model('Users_model');
+    $user = $ci->Users_model->getUserById($user_id);
+    if ($user->disable) {
+        $ci->response([
+            'status' => FALSE,
+            'message' => 'Usuário desabilitado. Contate administrador.'
         ], REST_Controller::HTTP_UNAUTHORIZED);
     }
 }
